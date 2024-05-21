40eb5702c1666d13a1c99447ad1e42b7a0de440c
User <User@DESKTOP-J7I50RK>
2024-05-20 21:17:18 -0400

    Add project files.

---
+++ Numeros_primos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace programa_principal
+{
+    class programa_principal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es un programa hecho por mi para saber si el numero que el usuario ingrese es primo o no\n");
+            Console.WriteLine("Ingrese un numero");
+
+            int numero_para_buscar_primo = int.Parse(Console.ReadLine());
+            int divisor = 1, contador = 0;
+            while(divisor <= numero_para_buscar_primo)
+            {
+                if(numero_para_buscar_primo % divisor == 0)
+                {
+                    contador++;
+                }    
+                divisor++;
+            }
+            if(contador == 2)
+            {
+                Console.WriteLine("El numero es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero no es primo");
+            }
+        }
+    }
+}
